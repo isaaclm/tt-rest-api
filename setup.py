c3950fdae40f3ac7200f479c95f9504424510d7a
isaaclm <isaaclm@users.noreply.github.com>
2023-11-09 18:15:19 +0000

    Updated version
    now 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 # Refer to the official Python Packaging Authority (PyPA) documentation for more details: https://packaging.python.org/tutorials/packaging-projects/
 setup(
     name='tt-rest-api',
-    version='0.1.0',
+    version='0.1.1',
     author='Isaac',
     description='An client for the Trading Technologies Rest API 2.0',
     url='https://github.com/isaaclm/tt-rest-api',
